60efbc57c729a22d7bcee4bf2c4cc55df21d1893
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-03-20 15:47:46 +0100

    ci: svelte test

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
+	let x: string = 0;
 </script>
 
-<div class="h-full w-full bg-black">
+<div class="h-full w-full bg-black" onclick={() => {}}>
 {@render children()}
 </div>
