7524e2f40fed83b109bbe859b47a35059bdca9f1
GitHub Action <action@github.com>
2025-03-20 14:53:09 +0000

    Fix code style issues with Prettier

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
-	let x: string = 0;
+  import '../app.css';
+  let { children } = $props();
+  let x: string = 0;
 </script>
 
 <div class="h-full w-full bg-black" onclick={() => {}}>
-{@render children()}
+  {@render children()}
 </div>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,16 +3,16 @@ import { defineConfig } from 'vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 
 export default defineConfig({
-	plugins: [sveltekit(), enhancedImages()],
-	server: {
-		port: 1420,
-		strictPort: true,
-		hmr: {
-			protocol: 'ws',
-			port: 1420
-		}
-	},
-	preview: {
-		port: 1420
-	}
+  plugins: [sveltekit(), enhancedImages()],
+  server: {
+    port: 1420,
+    strictPort: true,
+    hmr: {
+      protocol: 'ws',
+      port: 1420
+    }
+  },
+  preview: {
+    port: 1420
+  }
 });
