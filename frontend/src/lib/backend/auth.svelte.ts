b491612aa25cab74a792daba2f9fb12400b6b55c
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-02-01 15:02:24 +0100

    fix: request update

--- frontend/src/lib/backend/auth.svelte.ts
+++ frontend/src/lib/backend/auth.svelte.ts
@@ -1,20 +1,22 @@
 import { ResponseType, get, post } from "positron-components/backend";
 
 export const logout = async () => {
-  return await post<undefined>(
+  return await post(
     "/api/auth/logout",
-    ResponseType.None,
-    undefined,
   );
 };
 
 export const test_token = async () => {
-  let res = await get<boolean>("/api/auth/test_token", ResponseType.Json);
+  let res = await get<boolean>("/api/auth/test_token", {
+    res_type: ResponseType.Json,
+  });
   return typeof res === "boolean" && res;
 };
 
 export const get_oidc_url = async () => {
-  let res = await get<{ url: string }>('/api/auth/oidc_url', ResponseType.Json);
+  let res = await get<{ url: string }>('/api/auth/oidc_url', {
+    res_type: ResponseType.Json,
+  });
   if (typeof res === 'object' && 'url' in res) {
     return res.url;
   }
