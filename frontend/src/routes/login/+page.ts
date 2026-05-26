2823c01bc9dfddf218a774e0b54369ad692c1759
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-05-26 14:56:48 +0200

    fix: login page

--- frontend/src/routes/login/+page.ts
+++ frontend/src/routes/login/+page.ts
@@ -1,22 +1,13 @@
-import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
-import { SsoType, authConfig } from '$lib/client';
-import { getOidcUrl } from '$lib/backend/auth.svelte';
+import { authConfig } from '$lib/client';
 
-export const load: PageLoad = async ({ fetch, url }) => {
+export const load: PageLoad = ({ fetch, url }) => {
   const error = url.searchParams.get('error') || undefined;
   if (error) {
     return { error };
   }
   const skip = url.searchParams.get('skip') === 'true';
 
-  const { data: config } = await authConfig({ fetch });
-  if (config?.sso_type !== SsoType.NONE) {
-    const oidcUrl = await getOidcUrl();
-    if (oidcUrl && config?.instant_redirect && !skip) {
-      redirect(302, oidcUrl);
-    }
-    return { config, oidc_url: oidcUrl, skip };
-  }
+  const config = authConfig({ fetch }).then(({ data }) => data);
   return { config, skip };
 };
